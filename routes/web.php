30a28e0102abe3cab4d5a25281c3291a3ea8bde1
Your Name <rusik.bespalov54@gmail.com>
2021-08-15 21:32:40 +0300

    Rename route recursion and add project description

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get( '/', 'App\Http\Controllers\Comments\CommentsController@index')->name('home');
-Route::get( '/r', 'App\Http\Controllers\Comments\CommentsController@recursionOther')->name('recursion');
+Route::get( '/recursion', 'App\Http\Controllers\Comments\CommentsController@recursionOther')->name('recursion');
